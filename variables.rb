1e0ec17a3c71b9d520f6e277baf37c3223b0bc27
Logan Cole <emtlogancole@gmail.com>
2022-12-02 10:55:46 -0600

    Update Strings, add puts statements

--- variables.rb
+++ variables.rb
@@ -1,15 +1,24 @@
-Initial Stuff
+#Initial Stuff
 string1 = "First"
 string2 = "Second"
 string3 = "Not third haha tricked you"
+
+puts string1
 
 integer1 = 1
 integer2 = 2
 integer3 = 3
 
-float 1 = 1.1
-float 2 = 2.2
-float 3 = 4.20
+float1 = 1.1
+float2 = 2.2
+float3 = 4.20
 
 boolean = true
 boolean1 = false
+
+string1 = "Toothpaste"
+string2 = "Books"
+string3 = "Shoelace"
+
+puts string1
+puts string2
